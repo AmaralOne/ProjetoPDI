f65a6affeaecff21c00245f1ace7525e0e064077
AmaralOne <flaviofilho2908@gmail.com>
2020-06-15 20:36:44 -0300

    vesao final

--- projetoFinal/faces/main2.m
+++ projetoFinal/faces/main2.m
@@ -8,20 +8,18 @@ clear all, close all
 
     qtd_classes = 100;
     qtd_fotos = 14;
-    typeExtratorDeCaracteristicas = 2;
+    typeExtratorDeCaracteristicas = 2; % 2 - HOG; 3 - Somente PCA
     caminho = 'C:\Users\FlavioFilho\Documents\faculdade\9_Periodo\PDI\ProjetoFinal\ProjetoPDI\projetoFinal\faces\individuos\';
     cellSize = [4 4];
-    bag = [];  
-    %imds = imageDatastore(caminho,'IncludeSubfolders',true,'FileExtensions','.jpg');
-    %bag = bagOfFeatures(imds);
+    config = 1; % 1 - HOG COM O CORPOR; 2 - HOG COM A FACE; 3 - SOMENTE HOG
 vetor_acuracia = [];
 vetor_acuracia_por_classe = [];
-for k = 1:10
+for k = 1:1
 %% Ler o Comjunto de Treinamento e Teste
 disp("Teste "+k);
 porcertagem_teste = 0.3;
 
-[X_train, Y_train,X_test,Y_test,X_train_aux,X_teste_aux] = lerImgs2(qtd_classes,qtd_fotos,porcertagem_teste,typeExtratorDeCaracteristicas,caminho,cellSize,bag);
+[X_train, Y_train,X_test,Y_test,X_train_aux,X_teste_aux] = lerImgs2(qtd_classes,qtd_fotos,porcertagem_teste,typeExtratorDeCaracteristicas,caminho,cellSize,config);
 cd('C:\Users\FlavioFilho\Documents\faculdade\9_Periodo\PDI\ProjetoFinal\ProjetoPDI\projetoFinal\faces') % COLOQUE O ENDEREÇO !!!!
 %cd('C:\Users\Gideon\Desktop\PUC\Graduacao\9º Período\CMP1084 - Processamento Digital de Imagens\N2\Listas de exercícios\Lista 6')
 
@@ -69,6 +67,7 @@ end
 
 %% Testar Indivíduos separadamente
 i = 1;
+h = fspecial('average',[2 2]);
 while(i)
    im = input('Informe a imagem (entre aspas simples): '); %nome da pasta\imagem. Ex: '71-13.jpg'
    if(im == 0)
@@ -76,12 +75,31 @@ while(i)
    end
    %1º parâmetro: endereço da pasta de imagens
    x = imread(strcat(caminho,strcat('\',im)));   % COLOQUE O ENDEREÇO !!!!
-   x = imresize(x,0.5);
+   if(config == 1)
+      B = imfilter(x,h); % Filtro de Média.
+      B = imadjust(B,[],[],0.8); % Gamma correction.
+      x = imresize(B,0.5); % Redimensionar Imagem.
+      x = imcrop(x,[50 0 220 240]); % Recortar somente o corpo.
+   elseif(config == 2)
+       B = imfilter(x,h); % Filtro de Média.
+       B = imadjust(B,[],[],0.8); % Gamma correction.
+       x = imresize(B,0.5);% Redimensionar Imagem.
+       x = imcrop(x,[63 25 207 180]);% Recortar somente a face.
+   else
+      x = imresize(x,0.5);
+   end
    x = rgb2gray(x);
-   aux_x = ProjetarAmostra(x,mn,P,typeExtratorDeCaracteristicas,bag);
+   aux_x = ProjetarAmostra(x,mn,P,typeExtratorDeCaracteristicas);
    d = Classificar(PC, aux_x);
    figure;
-   imshowpair(reshape(X_train_aux(:,d),240,320),x,'montage')
+   if(config == 1)
+      imshowpair(reshape(X_train_aux(:,d),240,221),x,'montage')
+   elseif(config == 2)
+      imshowpair(reshape(X_train_aux(:,d),181,208),x,'montage')
+   else
+      imshowpair(reshape(X_train_aux(:,d),240,320),x,'montage')
+   end
+   
    
    clear im, clear x, clear d
     
